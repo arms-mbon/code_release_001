39e5578c337ba4c3e12f892ccfc3004e47f360f0
naurasd <62713295+naurasd@users.noreply.github.com>
2024-03-14 17:54:49 +0100

    Update 18S_merge_tables_first_step_for_data_paper.R

--- 18S_merge_tables_first_step_for_data_paper.R
+++ 18S_merge_tables_first_step_for_data_paper.R
@@ -7,7 +7,7 @@ library(readxl)
 library(stringr)
 library(openxlsx)    
 
-setwd("C:/Users/Nauras/OneDrive - University of Gothenburg/ARMS MBON/data paper/ARMS_data_paper_Nauras/Analysis/Nauras/18S")
+setwd("~/18S")
 
 # read Extended_final_tables of each sequencing run
 # One Gdynia sample was processed separately and therefore has its own table
